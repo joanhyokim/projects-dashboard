2276591e1599984da3cbcaddc51a03a202aadbf7
haotechou <haote.chou@liferay.com>
2016-12-06 14:17:07 -0800

    Modify ProblemLocalServiceImpl

--- projects-dashboard-problem/projects-dashboard-problem-service/src/main/java/com/liferay/projects/dashboard/problem/service/impl/ProblemLocalServiceImpl.java
+++ projects-dashboard-problem/projects-dashboard-problem-service/src/main/java/com/liferay/projects/dashboard/problem/service/impl/ProblemLocalServiceImpl.java
@@ -16,28 +16,69 @@ package com.liferay.projects.dashboard.problem.service.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.projects.dashboard.problem.model.Problem;
 import com.liferay.projects.dashboard.problem.service.base.ProblemLocalServiceBaseImpl;
 
+import java.util.Date;
+import java.util.List;
+
 /**
- * The implementation of the problem local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.projects.dashboard.problem.service.ProblemLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
  * @author Howie Chou
- * @see ProblemLocalServiceBaseImpl
- * @see com.liferay.projects.dashboard.problem.service.ProblemLocalServiceUtil
  */
 @ProviderType
 public class ProblemLocalServiceImpl extends ProblemLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.projects.dashboard.problem.service.ProblemLocalServiceUtil} to access the problem local service.
-	 */
+	public Problem addProblem(
+			long userId, long projectId, int type, String description,
+			int status, Date statusDate)
+		throws PortalException {
+
+		User user = userLocalService.getUser(userId);
+
+		long problemId = counterLocalService.increment();
+
+		Problem problem = problemPersistence.create(problemId);
+
+		Date now = new Date();
+
+		problem.setCompanyId(user.getCompanyId());
+		problem.setUserId(userId);
+		problem.setUserName(user.getFullName());
+		problem.setCreateDate(now);
+		problem.setModifiedDate(now);
+		problem.setProjectId(projectId);
+		problem.setType(type);
+		problem.setDescription(description);
+		problem.setStatus(status);
+		problem.setStatusDate(statusDate);
+
+		return problemPersistence.update(problem);
+	}
+
+	public List<Problem> getProblems(long projectId) throws PortalException {
+		return problemPersistence.findByprojectId(projectId);
+	}
+
+	public Problem updateProblem(
+			long problemId, long projectId, int type, String description,
+			int status, Date statusDate)
+		throws PortalException {
+
+		Problem problem = problemPersistence.findByPrimaryKey(problemId);
+
+		Date now = new Date();
+
+		problem.setModifiedDate(now);
+
+		problem.setProjectId(projectId);
+		problem.setType(type);
+		problem.setDescription(description);
+		problem.setStatus(status);
+		problem.setStatusDate(statusDate);
+
+		return problemPersistence.update(problem);
+	}
+
 }
