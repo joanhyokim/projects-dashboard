517ec10921303ff6e2c8da58e4605a20ce5b5d10
joanhyokim <joan.kim@liferay.com>
2016-12-06 15:57:47 -0800

    update action class

--- projects-dashboard-web/src/main/java/com/liferay/projects/dashboard/web/internal/portlet/action/EditProjectMVCActionCommand.java
+++ projects-dashboard-web/src/main/java/com/liferay/projects/dashboard/web/internal/portlet/action/EditProjectMVCActionCommand.java
@@ -16,7 +16,16 @@ package com.liferay.projects.dashboard.web.internal.portlet.action;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.projects.dashboard.project.service.ProjectLocalService;
 import com.liferay.projects.dashboard.web.internal.constants.ProjectsDashboardPortletKeys;
+
+import java.util.Date;
+
+import java.text.DateFormat;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -24,7 +33,7 @@ import javax.portlet.ActionResponse;
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Yury Butrymovich
+ * @author Joan H. Kim
  */
 @Component(
 	property = {
@@ -39,6 +48,44 @@ public class EditProjectMVCActionCommand extends BaseMVCActionCommand {
 	protected void doProcessAction(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
+
+		long projectId = ParamUtil.getLong(actionRequest, "projectId");
+
+		String name = ParamUtil.getString(actionRequest, "name");
+		String description = ParamUtil.getString(actionRequest, "description");
+		int priority = ParamUtil.getInteger(actionRequest, "priority");
+		Date expectedStartDate = ParamUtil.getDate(
+			actionRequest, "expectedStartDate",
+			DateFormat.getDateTimeInstance());
+		Date expectedEndDate = ParamUtil.getDate(
+			actionRequest, "expectedEndDate", DateFormat.getDateTimeInstance());
+		Date actualStartDate = ParamUtil.getDate(
+			actionRequest, "actualStartDate",
+			DateFormat.getDateTimeInstance());
+		Date actualEndDate = ParamUtil.getDate(actionRequest, "actualEndDate",
+			DateFormat.getDateTimeInstance());
+		int health = ParamUtil.getInteger(actionRequest, "health");
+		int status = ParamUtil.getInteger(actionRequest, "status");
+		long businessUnitId = ParamUtil.getLong(actionRequest, "businessUnitId");
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			actionRequest);
+
+		if (projectId > 0) {
+			_projectLocalService.updateProject(
+				projectId, name, description, priority, health,
+				expectedStartDate, expectedEndDate, actualStartDate,
+				actualEndDate, status);
+		}
+		else {
+			_projectLocalService.addProject(
+				serviceContext.getUserId(), businessUnitId, name, description,
+				priority, health, expectedStartDate, expectedEndDate,
+				actualStartDate, actualEndDate, status);
+		}
 	}
+
+	@Reference
+	private ProjectLocalService _projectLocalService;
 
 }
