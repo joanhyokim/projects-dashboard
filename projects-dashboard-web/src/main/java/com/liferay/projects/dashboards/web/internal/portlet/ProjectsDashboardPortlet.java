b499b6cf14b436a88d3c43ab5a1c02d74f1e779c
Ryan Park <ryan.park@liferay.com>
2016-12-06 12:33:41 -0800

    base jsps

---
+++ projects-dashboard-web/src/main/java/com/liferay/projects/dashboards/web/internal/portlet/ProjectsDashboardPortlet.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.projects.dashboard.web.internal.portlet;
+
+import com.liferay.projects.dashboard.web.internal.constants.ProjectsDashboardPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ryan Park
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=projects-dashboard-portlet",
+		"com.liferay.portlet.display-category=category.collaboration",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.layout-cacheable=false",
+		"com.liferay.portlet.preferences-owned-by-group=false",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Projects Dashboard",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=view",
+		"javax.portlet.name=" + ProjectsDashboardPortletKeys.PROJECTS_DASHBOARD,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class ProjectsDashboardPortlet extends MVCPortlet {
+}
