cb6b1a094088d2f13eed1033afe8c91b13b971e2
joanhyokim <joan.kim@liferay.com>
2016-12-06 13:10:47 -0800

    project services update

--- projects-dashboard-project/projects-dashboard-project-service/src/main/java/com/liferay/projects/dashboard/model/impl/ProjectModelImpl.java
+++ projects-dashboard-project/projects-dashboard-project-service/src/main/java/com/liferay/projects/dashboard/model/impl/ProjectModelImpl.java
@@ -119,13 +119,13 @@ public class ProjectModelImpl extends BaseModelImpl<Project>
 	public static final String DATA_SOURCE = "liferayDataSource";
 	public static final String SESSION_FACTORY = "liferaySessionFactory";
 	public static final String TX_MANAGER = "liferayTransactionManager";
-	public static final boolean ENTITY_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.osb.ldn.documentation.project.service.util.ServiceProps.get(
+	public static final boolean ENTITY_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.projects.dashboard.service.util.ServiceProps.get(
 				"value.object.entity.cache.enabled.com.liferay.projects.dashboard.model.Project"),
 			true);
-	public static final boolean FINDER_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.osb.ldn.documentation.project.service.util.ServiceProps.get(
+	public static final boolean FINDER_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.projects.dashboard.service.util.ServiceProps.get(
 				"value.object.finder.cache.enabled.com.liferay.projects.dashboard.model.Project"),
 			true);
-	public static final boolean COLUMN_BITMASK_ENABLED = GetterUtil.getBoolean(com.liferay.osb.ldn.documentation.project.service.util.ServiceProps.get(
+	public static final boolean COLUMN_BITMASK_ENABLED = GetterUtil.getBoolean(com.liferay.projects.dashboard.service.util.ServiceProps.get(
 				"value.object.column.bitmask.enabled.com.liferay.projects.dashboard.model.Project"),
 			true);
 	public static final long COMPANYID_COLUMN_BITMASK = 1L;
@@ -186,7 +186,7 @@ public class ProjectModelImpl extends BaseModelImpl<Project>
 		return models;
 	}
 
-	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(com.liferay.osb.ldn.documentation.project.service.util.ServiceProps.get(
+	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(com.liferay.projects.dashboard.service.util.ServiceProps.get(
 				"lock.expiration.time.com.liferay.projects.dashboard.model.Project"));
 
 	public ProjectModelImpl() {
