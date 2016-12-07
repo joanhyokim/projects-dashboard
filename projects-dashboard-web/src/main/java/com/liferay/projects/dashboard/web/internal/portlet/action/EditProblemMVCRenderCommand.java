e8bbe70915029b33d8ba982347d7bd8137bb9013
haotechou <haote.chou@liferay.com>
2016-12-06 16:06:05 -0800

    Implement MVC command classes for Problem entity

---
+++ projects-dashboard-web/src/main/java/com/liferay/projects/dashboard/web/internal/portlet/action/EditProblemMVCRenderCommand.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.projects.dashboard.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.projects.dashboard.web.internal.constants.ProjectsDashboardPortletKeys;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Howie Chou
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + ProjectsDashboardPortletKeys.PROJECTS_DASHBOARD,
+		"mvc.command.name=/edit_problem"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditProblemMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		return "/edit_problem.jsp";
+	}
+
+}
